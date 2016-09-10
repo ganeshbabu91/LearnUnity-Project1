fa3cab6526207592dc2c14d33e418f1eec9f1967
ganeshbabu <ganeshbabu.atech@gmail.com>
2016-09-10 14:22:22 -0500

    initial commit - full project

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+	public float speed = 10.5f;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Forward and backward movement (the character is moving along Z axis in the environment)
+		float z = Input.GetAxis ("Vertical") * speed;
+		// Left and right movement (the character is standing in same place but moving left and right)
+		float x = Input.GetAxis ("Horizontal") * speed;
+		// Smoothen the transition
+		z *= Time.deltaTime;
+		x *= Time.deltaTime;
+		// Apply the transformation
+		transform.Translate (x, 0, z);
+
+		// Change the cursor state while coming out of the play
+		if (Input.GetKeyDown ("escape")) {
+			Cursor.lockState = CursorLockMode.None;	
+		}
+	}
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	public void Click(){
+		Debug.Log("Button clicked");
+		GameObject gameObject = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		gameObject.name = "new cube";
+	}
+}
